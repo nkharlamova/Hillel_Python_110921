b8cad349cdac92ee928b46b12272d43cab3ca9f0
Natali <nata.kocto@gmail.com>
2021-11-23 20:24:53 +0200

    add homework 13.11.21

---
+++ checking filesreader.py
@@ -0,0 +1,17 @@
+# from homework_131121 import FilesReader
+#
+# filename = 'domains.txt'
+# read_file = FilesReader(filename)
+# domains = read_file.get_domains()
+# print(domains)
+#
+# filename = 'names.txt'
+# read_file = FilesReader(filename)
+# names = read_file.get_names()
+# print(names)
+#
+# filename = 'authors.txt'
+# read_file = FilesReader(filename)
+# dates = read_file.get_dates()
+# print(dates)
+
---
+++ checking_unit.py
@@ -0,0 +1,13 @@
+# from homework_131121 import Unit
+# unit_1 = Unit('Baraka', 'Tarkatan')
+# unit_2 = Unit('Jade', 'Edenia')
+#
+# unit_1.increase_unit_health()
+# unit_2.increase_unit_health()
+# unit_2.increase_unit_agility()
+# unit_1.increase_unit_intelligence()
+# unit_1.increase_unit_force()
+# unit_2.increase_unit_agility()
+#
+# print(unit_1)
+# print(unit_2)
